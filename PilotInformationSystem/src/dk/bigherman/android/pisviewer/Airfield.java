64c02deeb4d252111f18d73f59fef8ed967183ad
Nicolai Steffensen <nicolai.steffensen@gmail.com>
2013-06-06 13:52:08 +0200

    Intial Commit of start code

---
+++ PilotInformationSystem/src/dk/bigherman/android/pisviewer/Airfield.java
@@ -0,0 +1,55 @@
+package dk.bigherman.android.pisviewer;
+
+public class Airfield 
+{
+	private String icaoCode;
+	private double lat;
+	private double lng;
+	private int row;
+	private int col;
+	private String name;
+	
+	Airfield(String icao, double lat, double lng, String name)
+	{
+		this.icaoCode = icao;
+		this.lat = lat;
+		this.lng = lng;
+		this.name = name;
+	}
+	
+	public double getLat()
+	{
+		return lat;
+	}
+	
+	public double getLng()
+	{
+		return lng;
+	}
+	
+	public String getIcaoCode()
+	{
+		return icaoCode;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getCol() {
+		return col;
+	}
+
+	public void setCol(int col) {
+		this.col = col;
+	}
+}
